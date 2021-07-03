1c818a802c866b5c662dcca7890c94cf498a0fe4
omega1224 <84699294+omega1224@users.noreply.github.com>
2021-07-03 20:49:09 +0900

    Initial commit

---
+++ Assets/DestroySystem.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroySystem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        string s = other.gameObject.transform.tag;
+
+
+        Debug.Log("衝突した:"+other.gameObject.transform.tag);
+        if(s =="CoinTag" || s == "CarTag" || s == "TrafficConeTag")
+        {
+            Destroy(other.gameObject);
+
+        }  
+
+        
+    }
+    
+   
+}
